6af085f7a10a0ee05ec0d3743f12117da51d4ed3
natalia-s-belova <natalya.s.belova@gmail.com>
2023-06-09 17:35:36 +0200

    Set greeting back to Hi

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name} <3')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name} <3')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
